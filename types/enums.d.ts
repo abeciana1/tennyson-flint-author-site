d409286964af9b9d99498bcd8637c80fc7d203f5
abeciana1 <alex.beciana@gmail.com>
2025-02-23 21:40:43 -0700

    Created enums declaration file

---
+++ types/enums.d.ts
@@ -0,0 +1,48 @@
+export enum ROUNDED {
+  NONE = 'NONE',
+  SM = 'SM',
+  MD = 'MD',
+  LG = 'LG',
+  XL = 'XL',
+  FULL = 'FULL',
+  XXL = 'XXL',
+}
+
+export enum FONT_SIZE {
+  SM = 'SM',
+  MD = 'MD',
+  LG = 'LG',
+  XL = 'XL',
+  XXL = 'XXL',
+  XXXL = 'XXXL',
+  XXXXL = 'XXXXL',
+}
+
+export enum COLORS {
+  WHITE = 'WHITE',
+  BLACK = 'BLACK',
+  NAVY = 'NAVY',
+  GOLD = 'GOLD',
+  VIOLET = 'VIOLET',
+  RED = 'RED',
+  PINK = 'PINK'
+}
+
+export enum FONT_STYLE {
+  NORMAL = 'NORMAL',
+  ITALIC = 'ITALIC',
+  BOLD = 'BOLD',
+  SEMI_BOLD = 'SEMI_BOLD',
+  MEDIUM = 'MEDIUM'
+}
+
+export enum FONT_FAMILY {
+  SANS = 'SANS',
+  CINZEL_DEC = 'CINZEL_DEC',
+  CINZEL = 'CINZEL'
+}
+
+export enum FIELD_TYPE {
+  TEXT = 'text',
+  EMAIL = 'email',
+}
