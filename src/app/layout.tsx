b63dff93a91e35f65aa9547b1eeb024502733871
abeciana1 <alex.beciana@gmail.com>
2025-02-23 21:26:24 -0700

    Added imports and metadata config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,40 @@
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+import "./globals.css";
+import {
+  Open_Sans,
+  Cinzel,
+  Cinzel_Decorative
+} from 'next/font/google'
+import type { Metadata } from 'next'
+// import NavBar from '@/components/_navigation/NavBar'
+// import Footer from '@/components/_navigation/Footer'
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Tennyson Flint',
+    template: '%s | Tennyson Flint'
+  },
+  metadataBase: new URL('https://tennysonflint.com')
+}
+
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--open-sans'
+})
+
+const cinzel = Cinzel({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--cinzel'
+})
+
+const cinzelDecorative = Cinzel_Decorative({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--cinzel-dec'
+})
 
 export default function RootLayout({
   children,
@@ -8,7 +43,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${openSans.variable} ${cinzel.variable} ${cinzelDecorative.variable} antialiased relative flex flex-col`}
+      >{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
